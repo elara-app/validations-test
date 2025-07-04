510b2002e12ae2610e9b76c004279b430f063601
Julian Bermudez <julianbetov@gmail.com>
2025-07-04 17:51:01 -0500

    Add retention policy to HexColor validation annotation

--- src/main/java/com/elara/app/validation_utils_test/annotation/HexColor.java
+++ src/main/java/com/elara/app/validation_utils_test/annotation/HexColor.java
@@ -4,11 +4,10 @@ import com.elara.app.validation_utils_test.validator.HexColorValidator;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Documented
+@Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = HexColorValidator.class)
 @Target({ElementType.FIELD, ElementType.PARAMETER})
 public @interface HexColor {
